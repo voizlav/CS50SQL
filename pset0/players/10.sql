452ca2bd2acf9d2cfcd23a34d2a8c56269f501a6
Vojislav Trujkic <voizlav@gmail.com>
2023-10-30 07:41:44 +0100

    Update 10.sql to concat first and last name

--- pset0/players/10.sql
+++ pset0/players/10.sql
@@ -1,4 +1,4 @@
-SELECT "first_name" AS "Big Player First Name",
-"last_name" AS "Big Player Last Name" FROM "players"
+SELECT "first_name" || " " || "last_name" AS "Big Players"
+FROM "players"
 WHERE "weight" >= 300 AND "height" >= 70
-ORDER BY "first_name", "last_name" ASC;
+ORDER BY "first_name" ASC, "last_name" ASC;
